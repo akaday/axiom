1d03edc224dc4d7e109eac9b9dedf1ec3afc2ae0
aka <akadev8@outlook.com>
2024-11-05 06:06:53 +0100

    Update index.js
    Signed-off-by: aka <akadev8@outlook.com>

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 const { app, BrowserWindow } = require('electron');
 const path = require('path');
 const { exec } = require('child_process');
-const crypto = require('crypto'); // Adding crypto for secure random values
+const crypto = require('crypto'); // Secure random number generator
 
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
 if (require('electron-squirrel-startup')) {
@@ -9,7 +9,6 @@ if (require('electron-squirrel-startup')) {
 }
 
 const createWindow = () => {
-  // Create the browser window.
   const mainWindow = new BrowserWindow({
     width: 800,
     height: 600,
@@ -18,10 +17,7 @@ const createWindow = () => {
     },
   });
 
-  // and load the index.html of the app.
   mainWindow.loadFile(path.join(__dirname, 'index.html'));
-
-  // Open the DevTools.
   mainWindow.webContents.openDevTools();
 };
 
@@ -47,14 +43,9 @@ exec('python ai_script.py', (error, stdout, stderr) => {
   console.log(`Stdout: ${stdout}`);
 });
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
 app.whenReady().then(() => {
   createWindow();
 
-  // On macOS it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
   app.on('activate', () => {
     if (BrowserWindow.getAllWindows().length === 0) {
       createWindow();
@@ -62,14 +53,8 @@ app.whenReady().then(() => {
   });
 });
 
-// Quit when all windows are closed, except on macOS. There, it's common
-// for applications and their menu bar to stay active until the user quits
-// explicitly with Cmd + Q.
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') {
     app.quit();
   }
 });
-
-// In this file, you can include the rest of your app's specific main process
-// code. You can also put them in separate files and import them here.

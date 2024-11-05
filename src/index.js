f89a601322e54264219ac3a6e15c40da63420f5e
aka <akadev8@outlook.com>
2024-11-05 02:58:04 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
 const { app, BrowserWindow } = require('electron');
-const path = require('path');
+const path = require('node:path');
 const { exec } = require('child_process');
 
-function createWindow() {
-  const win = new BrowserWindow({
+// Handle creating/removing shortcuts on Windows when installing/uninstalling.
+if (require('electron-squirrel-startup')) {
+  app.quit();
+}
+
+const createWindow = () => {
+  // Create the browser window.
+  const mainWindow = new BrowserWindow({
     width: 800,
     height: 600,
     webPreferences: {
@@ -11,8 +17,12 @@ function createWindow() {
     },
   });
 
-  win.loadFile('index.html');
-}
+  // and load the index.html of the app.
+  mainWindow.loadFile(path.join(__dirname, 'index.html'));
+
+  // Open the DevTools.
+  mainWindow.webContents.openDevTools();
+};
 
 // Execute Python script for AI tasks
 exec('python ai_script.py', (error, stdout, stderr) => {
@@ -27,9 +37,14 @@ exec('python ai_script.py', (error, stdout, stderr) => {
   console.log(`Stdout: ${stdout}`);
 });
 
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+// Some APIs can only be used after this event occurs.
 app.whenReady().then(() => {
   createWindow();
 
+  // On OS X it's common to re-create a window in the app when the
+  // dock icon is clicked and there are no other windows open.
   app.on('activate', () => {
     if (BrowserWindow.getAllWindows().length === 0) {
       createWindow();
@@ -37,8 +52,14 @@ app.whenReady().then(() => {
   });
 });
 
+// Quit when all windows are closed, except on macOS. There, it's common
+// for applications and their menu bar to stay active until the user quits
+// explicitly with Cmd + Q.
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') {
     app.quit();
   }
 });
+
+// In this file you can include the rest of your app's specific main process
+// code. You can also put them in separate files and import them here.
